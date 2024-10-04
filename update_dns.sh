f0935390880480783a8c225fa1b3a36f91c36701
guoziyou <147931291+guoziyou@users.noreply.github.com>
2024-10-04 18:48:12 +0800

    Update update_dns.sh

--- update_dns.sh
+++ update_dns.sh
@@ -10,7 +10,7 @@ DOMAIN="armbian.fa666.us.kg"
 IPV6=$(ip -6 addr show | grep "inet6" | grep "global" | awk '{print $2}' | cut -d '/' -f 1)
 
 # 更新 Cloudflare DNS 记录
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
-     -H "Authorization: Bearer $API_TOKEN" \
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/048eee499101d0d41340a7b31cce8835/dns_records/54f1081dc5484ce23f54e0f7c91b6a48" \
+     -H "Authorization: Bearer b08d39dd2fdf5cb859411a0e518a12da8ce74" \
      -H "Content-Type: application/json" \
-     --data "{\"type\":\"AAAA\",\"name\":\"$DOMAIN\",\"content\":\"$IPV6\",\"ttl\":120,\"proxied\":false}"
+     --data "{\"type\":\"AAAA\",\"name\":\"armbian.fa666.us.kg\",\"content\":\"240e:33d:978e:a500:33a6:d9da:8a4a:856d\",\"ttl\":120,\"proxied\":false}"
